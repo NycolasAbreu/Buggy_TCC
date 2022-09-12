905a59eb2666caf4da34c544961c61a91c4f7bec
Nycolas Coelho de Abreu <nyco_abreu@hotmail.com>
2022-09-12 19:02:15 -0300

    class motor

--- Buggy/motor.py
+++ Buggy/motor.py
@@ -14,56 +14,56 @@ class Motor:
     pwm_freq = 1000
     
     def __init__(self):
-        
-        self.m1  = PWM(Pin(M1), pwm_freq)
-        self.m1n = PWM(Pin(M1N), pwm_freq)
-        self.m2  = PWM(Pin(M2), pwm_freq)
-        self.m2n = PWM(Pin(M2N), pwm_freq)
-        
+        self.m1  = PWM(Pin(self.M1), self.pwm_freq)
+        self.m1n = PWM(Pin(self.M1N), self.pwm_freq)
+        self.m2  = PWM(Pin(self.M2), self.pwm_freq)
+        self.m2n = PWM(Pin(self.M2N), self.pwm_freq)
+
         self.parar()
         
-    def parar(self):        
-        # Todos desligados
-        self.m1.deinit()
-        self.m1n.deinit()
-        self.m2.deinit()
-        self.m2n.deinit()
+    def parar(self):
+        self.m1.duty(0)
+        self.m1n.duty(0)
+        self.m2.duty(0)
+        self.m2n.duty(0)
         
-    def porcentagem_duty(porcentagem):
+    def porcentagem_duty(self, porcentagem):
         return int((porcentagem * 1023) / 100)
         
     def mover_frente(self, porcentagem):
-        
         self.parar()
-        
         if (porcentagem > 0) and (porcentagem <= 100):
             duty = self.porcentagem_duty(porcentagem)
             self.m1.duty(duty)
             self.m2.duty(duty)
         
     def mover_re(self, porcentagem):
-        
         self.parar()
-        
         if (porcentagem > 0) and (porcentagem <= 100):
             duty = self.porcentagem_duty(porcentagem)
             self.m1n.duty(duty)
             self.m2n.duty(duty)
         
     def mover_esquerda(self, porcentagem):
-        
         self.parar()
-        
         if (porcentagem > 0) and (porcentagem <= 100):
             duty = self.porcentagem_duty(porcentagem)
             self.m1.duty(duty)
             self.m2n.duty(duty)
         
     def mover_direita(self, porcentagem):
-        
         self.parar()
-        
         if (porcentagem > 0) and (porcentagem <= 100):
             duty = self.porcentagem_duty(porcentagem)
             self.m1n.duty(duty)
             self.m2.duty(duty)
+
+if __name__ == '__main__':
+    motores = Motor()
+    motores.mover_frente(100)
+    time.sleep(3)
+    motores.mover_re(100)
+    time.sleep(3)
+    motores.mover_direita(100)
+    time.sleep(3)
+    motores.mover_esquerda(100)
